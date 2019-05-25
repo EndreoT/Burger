66c0290bbbeb6d2111a9d71f6904da2999b77402
EndreoT <endreotm@gmail.com>
2019-05-24 17:25:40 -0700

    begin adding jQuery to handle view logic

---
+++ public/assets/javascript/javascript.js
@@ -0,0 +1,57 @@
+
+$(function () {
+  // $(".change-sleep").on("click", function(event) {
+  //   var id = $(this).data("id");
+  //   var newSleep = $(this).data("newsleep");
+
+  //   var newSleepState = {
+  //     sleepy: newSleep
+  //   };
+
+  //   // Send the PUT request.
+  //   $.ajax("/api/cats/" + id, {
+  //     type: "PUT",
+  //     data: newSleepState
+  //   }).then(
+  //     function() {
+  //       console.log("changed sleep to", newSleep);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+
+  $("#submit-burger").click(function (event) {
+    event.preventDefault();
+
+    const burger = {
+      name: $("#burger-name").val().trim(),
+      sleepy: $("[name=devoured]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: burger
+    }).then(
+      function () {
+        location.reload();
+      }
+    );
+  });
+
+  // $(".delete-cat").on("click", function(event) {
+  //   var id = $(this).data("id");
+
+  //   // Send the DELETE request.
+  //   $.ajax("/api/cats/" + id, {
+  //     type: "DELETE"
+  //   }).then(
+  //     function() {
+  //       console.log("deleted cat", id);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+});
